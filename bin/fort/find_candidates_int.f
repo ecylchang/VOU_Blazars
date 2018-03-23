fe990359addeca17399a7fcea4a217b454608229
Yu Ling Chang <yulingchang@macbook>
2018-03-23 21:01:28 +0100

    Solved the multiple X-ray sources problem in intermediate phase

--- bin/fort/find_candidates_int.f
+++ bin/fort/find_candidates_int.f
@@ -1,17 +1,18 @@
       PROGRAM find_candidates_int
 
       IMPLICIT none
-      integer i,j,k,iuv,igam,i4p8,irr,ixx,in,lenact,length,lu_in,ier,icand,filen,is,ie,xpts(2000)
+      integer i,j,k,m,s,iuv,igam,i4p8,irr,ixx,in,lenact,length,lu_in,ier,icand,filen,is,ie,xpts(2000)
       integer ii1,ii2,ipass,isource,pass2(4000),rr_type(2000),xx_type(2000),xxot_type(10,2000)
-      integer nrep(2000),xrayrepeat(2000,2000),ixxss,ixxrep,track(2000)
+      integer nrep(2000),back(2000,2000),ixxss,ixxrep,track(2000),repnumber(2000),nnuvx,posind(2000)
       real*8 ra,dec,dist,ra_uv(10000),dec_uv(10000),ra_gam(50),dec_gam(50),ra_4p8(500),dec_4p8(500)
       real*8 ra_rr(2000),dec_rr(2000),ra_xx(2000),dec_xx(2000),ra_center,dec_center
+      real*8 ra_xxss(2000),dec_xxss(2000)
       real*4 nh,flux,uflux,lflux,epos,freq,code,radius,flux2nufnu_4p8,fdens,nudens
       real*4 frequency_rr(2000),flux_rr(2000),FluxL_rr(2000),FluxU_rr(2000),poserr_rr(2000)
       real*4 frequency_xx(2000),flux_xx(2000),FluxL_xx(2000),FluxU_xx(2000),poserr_xx(2000)
       real*4 frequency_xxot(10,2000),flux_xxot(10,2000),FluxL_xxot(10,2000),FluxU_xxot(10,2000)
       real*4 frequency_4p8(500),flux_4p8(500),FluxL_4p8(500),FluxU_4p8(500),poserr_xxot(10,2000)
-      real*4 poserr_4p8(500),Ferr_4p8(500),poserr_uv(10000)
+      real*4 poserr_4p8(500),Ferr_4p8(500),poserr_uv(10000),poserr_xxss(2000)
       real*4 frequency_uv(10000,2),flux_uv(10000,2),FluxL_uv(10000,2),FluxU_uv(10000,2)
       real*4 uvmag(10000,2),uvmagerr(10000,2),slope_gam(50),specerr_gam(50)
       real*4 frequency_gam(50),flux_gam(50),FluxL_gam(50),FluxU_gam(50),poserr_gam(50),Ferr_gam(50)
@@ -221,99 +222,146 @@ c      write(*,*) i4p8,iuv,igam
          if (i .ne. 1) then
             do j=1,i-1
                call DIST_SKY(ra_xx(i),dec_xx(i),ra_xx(j),dec_xx(j),dist)
-               if (dist*3600. .lt. 15.) then
+               if (dist*3600. .lt. 12.) then
                   !write(*,*) 'nearby X-ray',ra_xx(i),dec_xx(i),xx_type(ixx)
                   ixxrep=ixxrep+1
 c                  xrayrepeat(nrep(i),i)=j
                   track(i)=track(j)
                   nrep(track(i))=nrep(track(i))+1
-c                  write(*,*) i,track(i),nrep(track(i))
+                  repnumber(i)=nrep(track(i))
+c                  write(*,*) i,track(i),xx_type(i),poserr_xx(i),nrep(track(i))
                   goto 97
                endif
             enddo
          endif
          ixxss=ixxss+1
          track(i)=ixxss
-c         write(*,*) i,track(i)
+         nrep(track(i))=1
+         repnumber(i)=nrep(track(i))
+c         write(*,*) i,track(i),xx_type(i),poserr_xx(i)!,back(i,repnumber(i))
 97    continue
       enddo
       if (ixxss+ixxrep .ne. ixx ) write(*,*) 'Warning! may have the wrong number.'
 
+c xrt 1sxps chandra
+c bmw xmm xmmsl
+c rass wga ipc
+c      write(*,*) '========================'
+      do i=1,ixxss
+         do j=1,ixx
+            if (track(j) .eq. i) then
+               back(i,repnumber(j))=j
+               if (repnumber(j) .eq. 1) then
+                  poserr_xxss(track(j))=poserr_xx(j)
+                  ra_xxss(track(j))=ra_xx(j)
+                  dec_xxss(track(j))=dec_xx(j)
+                  posind(track(j))=j
+               else
+c                  write(*,*) j,track(j),xx_type(j),poserr_xx(j),repnumber(j)
+                  if ((xx_type(j) .eq. 15) .or. (xx_type(j) .eq. 19) .or. (xx_type(j) .eq. 18)) then
+                     if (poserr_xx(j) .lt. poserr_xxss(track(j)) ) then
+                        ra_xxss(track(j))=ra_xx(j)
+                        dec_xxss(track(j))=dec_xx(j)
+                        posind(track(j))=j
+                     endif
+                  else if ((xx_type(j) .eq. 11) .or. (xx_type(j) .eq. 12) .or. (xx_type(j) .eq. 17)) then
+                     if (poserr_xx(j) .lt. poserr_xxss(track(j)) ) then
+                        ra_xxss(track(j))=ra_xx(j)
+                        dec_xxss(track(j))=dec_xx(j)
+                        posind(track(j))=j
+                     endif
+                  else if ((xx_type(j) .eq. 13) .or. (xx_type(j) .eq. 14) .or. (xx_type(j) .eq. 16)) then
+                     if (poserr_xx(j) .lt. poserr_xxss(track(j)) ) then
+                        ra_xxss(track(j))=ra_xx(j)
+                        dec_xxss(track(j))=dec_xx(j)
+                        posind(track(j))=j
+                     endif
+                  endif
+               endif
+            endif
+         enddo
+c         write(*,*) i,posind(i),ra_xxss(i),dec_xxss(i),nrep(i),back(i,1:nrep(i))
+      enddo
+
       open(12,file='Intermediate_out.txt',status='unknown',iostat=ier)
       ipass=0
-      do i=1,icand
+      do i=1,irr
          ii1=0
          ii2=0
-         if (i .le. irr) then
-            do j=1,iuv
-               call DIST_SKY(ra_rr(i),dec_rr(i),ra_uv(j),dec_uv(j),dist)
-               if ( dist*3600. .le. poserr_rr(i)*1.3 ) then
-                  if (flux_uv(j,1) .ne. 0.) then
-                     aruv = 1.-log10(flux_rr(i)/flux_uv(j,1))/log10(frequency_rr(i)/frequency_uv(j,1))
-                  else
-                     aruv = 1.-log10(flux_rr(i)/flux_uv(j,2))/log10(frequency_rr(i)/frequency_uv(j,2))
-                  endif
-                  if (aruv .le. 0.75 ) then
-                     ii1=ii1+1 !!!remove UV-r slope strange source 0.85
-                     if (ii1 .eq. 1) write(*,'(a,i4,a)') "----------------------------------------"
-                     if (ii1 .eq. 1) write(*,'(f9.5,2x,f9.5,a,f9.3,a)') ra_rr(i),dec_rr(i)," radio source ",
-     &                 flux_rr(i)/frequency_rr(i)/1.E-26," mJy"
-                     write(*,'("GALEX : ",2(f6.3,2x),10x,f7.3," arcsec away")') uvmag(j,1),uvmag(j,2),dist*3600.
-                     write(*,'(6x,"radio-UV slope: ",f6.3)') aruv
-                  endif
+         do j=1,iuv
+            call DIST_SKY(ra_rr(i),dec_rr(i),ra_uv(j),dec_uv(j),dist)
+            if ( dist*3600. .le. poserr_rr(i)*1.3 ) then
+               if (flux_uv(j,1) .ne. 0.) then
+                  aruv = 1.-log10(flux_rr(i)/flux_uv(j,1))/log10(frequency_rr(i)/frequency_uv(j,1))
+               else
+                  aruv = 1.-log10(flux_rr(i)/flux_uv(j,2))/log10(frequency_rr(i)/frequency_uv(j,2))
                endif
-            enddo
-            do j=1,i4p8
-               call DIST_SKY(ra_rr(i),dec_rr(i),ra_4p8(j),dec_4p8(j),dist)
-               if (dist*3600. .le. poserr_4p8(j)*1.3 ) then
-                  alphar = 1.-log10(flux_rr(i)/flux_4p8(j))/log10(frequency_rr(i)/frequency_4p8(j))
-                  if (alphar .le. 0.7 ) then
-                     ii2=ii2+1 !!!remove radio extended sources
-                     if ((ii2 .eq. 1) .and. (ii1 .eq. 0))
-     &                   write(*,'(a,i4,a)') "----------------------------------------"
-                     if ((ii2 .eq. 1) .and. (ii1 .eq. 0)) write(*,'(f9.5,2x,f9.5,a,f9.3,a)')
-     &                  ra_rr(i),dec_rr(i)," radio source ",flux_rr(i)/frequency_rr(i)/1.E-26," mJy"
-                     write(*,'(a,f9.3," mJy",10x,f7.3," arcsec away")')
-     &                  type_4p8(j),flux_4p8(j)/flux2nufnu_4p8,dist*3600.
-                     write(*,'("radio slope: ",f6.3)') alphar
-                  endif
+               if (aruv .le. 0.75 ) then
+                  ii1=ii1+1 !!!remove UV-r slope strange source 0.85
+                  if (ii1 .eq. 1) write(*,'(a,i4,a)') "----------------------------------------"
+                  if (ii1 .eq. 1) write(*,'(f9.5,2x,f9.5,a,f9.3,a)') ra_rr(i),dec_rr(i)," radio source ",
+     &              flux_rr(i)/frequency_rr(i)/1.E-26," mJy"
+                  write(*,'("GALEX : ",2(f6.3,2x),10x,f7.3," arcsec away")') uvmag(j,1),uvmag(j,2),dist*3600.
+                  write(*,'(6x,"radio-UV slope: ",f6.3)') aruv
                endif
-            enddo
-            if (ii1+ii2 .gt. 0.) then
-               ipass=ipass+1
-               CALL RXgraphic_code(flux_rr(i)/frequency_rr(i)/1.E-26,'R',code)
-               write (12,'(f9.5,2x,f9.5,2x,i6)') ra_rr(i),dec_rr(i),int(code)
-               pass2(ipass)=i
             endif
-         else if (i .gt. irr ) then
+         enddo
+         do j=1,i4p8
+            call DIST_SKY(ra_rr(i),dec_rr(i),ra_4p8(j),dec_4p8(j),dist)
+            if (dist*3600. .le. poserr_4p8(j)*1.3 ) then
+               alphar = 1.-log10(flux_rr(i)/flux_4p8(j))/log10(frequency_rr(i)/frequency_4p8(j))
+               if (alphar .le. 0.7 ) then
+                  ii2=ii2+1 !!!remove radio extended sources
+                  if ((ii2 .eq. 1) .and. (ii1 .eq. 0))
+     &                write(*,'(a,i4,a)') "----------------------------------------"
+                  if ((ii2 .eq. 1) .and. (ii1 .eq. 0)) write(*,'(f9.5,2x,f9.5,a,f9.3,a)')
+     &             ra_rr(i),dec_rr(i)," radio source ",flux_rr(i)/frequency_rr(i)/1.E-26," mJy"
+                  write(*,'(a,f9.3," mJy",10x,f7.3," arcsec away")')
+     &             type_4p8(j),flux_4p8(j)/flux2nufnu_4p8,dist*3600.
+                  write(*,'("radio slope: ",f6.3)') alphar
+               endif
+            endif
+         enddo
+         if (ii1+ii2 .gt. 0.) then
+            ipass=ipass+1
+            CALL RXgraphic_code(flux_rr(i)/frequency_rr(i)/1.E-26,'R',code)
+            write (12,'(f9.5,2x,f9.5,2x,i6)') ra_rr(i),dec_rr(i),int(code)
+            pass2(ipass)=i
+         endif
+      enddo
+
+      do i=1,ixxss
+         ii1=0
+         do m=1,nrep(i)
+            k=back(i,m)
+            nnuvx=0
             do j=1,iuv
-               call DIST_SKY(ra_xx(i-irr),dec_xx(i-irr),ra_uv(j),dec_uv(j),dist)
-               if ((dist*3600. .le. poserr_xx(i-irr)*1.3 ) .and. (poserr_xx(i-irr) .le. 15.)
-     &              .and. (flux_xx(i-irr) .ne. 0. ) ) then
+               call DIST_SKY(ra_xx(k),dec_xx(k),ra_uv(j),dec_uv(j),dist)
+               if ((dist*3600. .le. poserr_xx(k)*1.3 ) .and. (poserr_xx(k) .le. 15.)
+     &              .and. (flux_xx(k) .ne. 0. ) ) then
+                  nnuvx=nnuvx+1
                   if (flux_uv(j,1) .ne. 0.) then
-                     auvx = 1.-log10(flux_uv(j,1)/flux_xx(i-irr))/log10(frequency_uv(j,1)/frequency_xx(i-irr))
+                     auvx = 1.-log10(flux_uv(j,1)/flux_xx(k))/log10(frequency_uv(j,1)/frequency_xx(k))
                   else
-                     auvx = 1.-log10(flux_uv(j,2)/flux_xx(i-irr))/log10(frequency_uv(j,2)/frequency_xx(i-irr))
+                     auvx = 1.-log10(flux_uv(j,2)/flux_xx(k))/log10(frequency_uv(j,2)/frequency_xx(k))
                   endif
-!                  write(*,*) i,auvx
-                  if (auvx .le. 1.4 ) then
+                  if (auvx .le. 1.4) then
                      ii1=ii1+1 !!!remove UV-x slope strange source 0.85
                      if (ii1 .eq. 1) write(*,'(a,i4,a)') "----------------------------------------"
-                     if (ii1 .eq. 1) write(*,'(f9.5,2x,f9.5,a,es10.3)') ra_xx(i-irr),dec_xx(i-irr),
-     &            " X-ray source with 1 keV flux ",flux_xx(i-irr)
+                     if ((ii1 .eq. 1) .or. ((m .ne. 1) .and. (nnuvx .eq. 1)))
+     $                  write(*,'(f9.5,2x,f9.5,a,es10.3)')  ra_xx(k),
+     $                  dec_xx(k)," X-ray source with 1 keV flux ",flux_xx(k)
                      write(*,'("GALEX : ",2(f6.3,2x),10x,f7.3," arcsec away")') uvmag(j,1),uvmag(j,2),dist*3600.
                      write(*,'(6x,"UV-X-ray slope: ",f6.3)') auvx
                   endif
-                 !ra_uvmat(iuvmatch)=ra_uv(j)
-                 !dec_uvmat(iuvmatch)=dec_uv(j)
                endif
             enddo
-            if (ii1 .gt. 0.) then
-               ipass=ipass+1
-               CALL RXgraphic_code(flux_xx(i-irr),'X',code)
-               write (12,'(f9.5,2x,f9.5,2x,i6)') ra_xx(i-irr),dec_xx(i-irr),int(code)
-               pass2(ipass)=i
-            endif
+         enddo
+         if (ii1 .gt. 0.) then
+             ipass=ipass+1
+             CALL RXgraphic_code(flux_xx(posind(i)),'X',code)
+             write (12,'(f9.5,2x,f9.5,2x,i6)') ra_xx(posind(i)),dec_xx(posind(i)),int(code)
+             pass2(ipass)=i+irr
          endif
       enddo
 
@@ -325,23 +373,28 @@ c         write(*,*) i,track(i)
          print *,achar(27),'[31;1m Unfortunatelly, no any candidates were found',achar(27),'[0m'
          stop
       endif
+c      write(*,*) pass2(1:ipass)
 
       do i=1,ipass
          k=pass2(i)
-         if (i+isource .ne. 1) write(12,*) "===================="
          if (k .le. irr) then
+            if (i+isource .ne. 1) write(12,*) "===================="
             write(12,'(i4,2x,a,2(2x,f9.5),2x,a,2x,i2)') i+isource,"matched source",
      &         ra_rr(k),dec_rr(k),'source type',int(code/10000)
             write(12,'(4(es10.3,2x),2(f9.5,2x),f7.3,2x,i2)') frequency_rr(k),flux_rr(k),FluxU_rr(k),
      &          FluxL_rr(k),ra_rr(k),dec_rr(k),poserr_rr(k),rr_type(k)
          else
-            write(12,'(i4,2x,a,2(2x,f9.5),2x,a,2x,i2)') i+isource,"matched source",
-     &         ra_xx(k-irr),dec_xx(k-irr),'source type',int(code/10000)
-            write(12,'(4(es10.3,2x),2(f9.5,2x),f7.3,2x,i2)') frequency_xx(k-irr),flux_xx(k-irr),
-     &         FluxU_xx(k-irr),FluxL_xx(k-irr),ra_xx(k-irr),dec_xx(k-irr),poserr_xx(k-irr),xx_type(k-irr)
-            do j=1,xpts(k-irr)
-               write(12,'(4(es10.3,2x),i2)') frequency_xxot(j,k-irr),flux_xxot(j,k-irr),
-     &            FluxU_xxot(j,k-irr),FluxL_xxot(j,k-irr),xxot_type(j,k-irr)
+            do s=1,nrep(k-irr)
+               m=back(k-irr,s)
+               if (i+isource .ne. 1) write(12,*) "===================="
+               write(12,'(i4,2x,a,2(2x,f9.5),2x,a,2x,i2)') i+isource,"matched source",
+     &           ra_xx(m),dec_xx(m),'source type',int(code/10000)
+               write(12,'(4(es10.3,2x),2(f9.5,2x),f7.3,2x,i2)') frequency_xx(m),flux_xx(m),
+     &           FluxU_xx(m),FluxL_xx(m),ra_xx(m),dec_xx(m),poserr_xx(m),xx_type(m)
+               do j=1,xpts(m)
+                  write(12,'(4(es10.3,2x),i2)') frequency_xxot(j,m),flux_xxot(j,m),
+     &             FluxU_xxot(j,m),FluxL_xxot(j,m),xxot_type(j,m)
+               enddo
             enddo
          endif
       enddo
